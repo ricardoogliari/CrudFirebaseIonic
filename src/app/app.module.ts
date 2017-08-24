433623217d333954d2279edfce736ba7280bf705
ricardoogliari <rogliariping@gmail.com>
2017-08-24 13:01:57 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,29 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+//IMPORTANTE
+//menção aos sites onde encontrei o conhecimento
+//http://www.fabricadecodigo.com/como-criar-um-crud-com-ionic-2-e-firebase/
+//https://github.com/angular/angularfire2
+
+//configuração do banco de dados na nuvem
+//Fui em http://console.firebase.google.com/, criei um aplicativo
+//na parte de configurações fui em 
+//"adicionar o Firebase ao seu Arquivo da Web"
+const firebaseConfig = {
+    apiKey: "AIzaSyCB-74Q-x4UzfVxeiAXgvkqBQc9Un7Qy9k",
+    authDomain: "arduinofirebase-21dc5.firebaseapp.com",
+    databaseURL: "https://arduinofirebase-21dc5.firebaseio.com",
+    projectId: "arduinofirebase-21dc5",
+    storageBucket: "arduinofirebase-21dc5.appspot.com",
+    messagingSenderId: "355951549273"
+};
 
 @NgModule({
   declarations: [
@@ -13,6 +34,10 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    //o primeiro import inicializa o firebase com as
+    //configurações necessárias
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//imports das bibliotecas do AngularFire2 necessárias
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items: FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+    //quando a lista é carregada o Angular se encarrega do resto
+    //a lista está no HTML e será populada asincronamente
+    this.items = db.list('/');
   }
 
 }
