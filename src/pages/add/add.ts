9c2af14bad08e64e8fea99d760a9d208ff12ca2f
ricardoogliari <rogliariping@gmail.com>
2017-08-27 18:43:32 -0300

    remover e editar pessoa prontos

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 //imports das bibliotecas do AngularFire2 necessárias
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
@@ -7,21 +7,40 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
   templateUrl: 'add.html'
 })
 export class AddPage {
+  titulo: String = "Nova Pessoa";
   nome: string;
   idade: string;
+  next: Number;
+  pessoa;
 
   items: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController, private db: AngularFireDatabase) {
+  constructor(
+      public navCtrl: NavController, 
+      db: AngularFireDatabase,
+      navParams: NavParams) {
+    this.next = navParams.get("next");
+    this.pessoa = navParams.get("pessoa");
+
+    if (this.pessoa != null){
+      //se o objeto pessoa recebido não for nulo, significa que essa tela
+      //será usada para editar uma pessoa existente
+      this.nome = this.pessoa.nome;
+      this.idade = this.pessoa.idade;
+      this.titulo = "Editar " + this.pessoa.nome;
+    }
+
     this.items = db.list('/');
   }
 
   salvar(){
-    this.items.push(
+    //atualiza uma chave.. se a chave existe edita, senão, cria um nova pessoa
+    this.items.update("" + this.next, 
         {   
             'nome': this.nome, 
             'idade': parseInt(this.idade)
         });
+    this.navCtrl.pop();
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 //imports das bibliotecas do AngularFire2 necessárias
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { AddPage } from '../add/add';
 import { AlertController } from 'ionic-angular';
 
@@ -10,20 +10,30 @@ import { AlertController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  size: Number;
   items: FirebaseListObservable<any[]>;
   constructor(public navCtrl: NavController, 
-            db: AngularFireDatabase,
+            public db: AngularFireDatabase,
             public alertCtrl: AlertController) {
     //quando a lista é carregada o Angular se encarrega do resto
     //a lista está no HTML e será populada asincronamente
     this.items = db.list('/');
+
+    //como o list é assíncrono, quando ele conseguir ler os dados ele verifica quantos
+    //elementos tem, com isso, sei a chave do próximo elemento
+    this.items.subscribe(sub => {
+      this.size = sub.length;
+    });
   }
 
   addPerson(){
-    this.navCtrl.push(AddPage);
+    //passa para a próxima tela a chave da próxima pessoa
+    this.navCtrl.push(AddPage, {
+      next: this.size
+    });
   }
 
-  clique(pessoa){
+  clique(pessoa, key){
     let confirm = this.alertCtrl.create({
       title: 'Ação',
       message: 'Qual a ação que deseja fazer com o usuário ' + pessoa.nome + "?",
@@ -31,13 +41,20 @@ export class HomePage {
         {
           text: 'Excluir',
           handler: () => {
-            console.log('Disagree clicked');
+            //remove um objeto pela key
+            this.items.remove("" + key);
           }
         },
         {
           text: 'Ver',
           handler: () => {
-            console.log('Agree clicked');
+            //usa a mesma tela de nova pessoa, mas passando o objeto por parâmetro
+            //dessa forma, a tela de destino sabe que é edição e não uma nova
+            //pessoa
+            this.navCtrl.push(AddPage, {
+              next: key,
+              pessoa: pessoa
+            });
           }
         }
       ]
